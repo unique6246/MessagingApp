230b646873a0ed989fbabf68642f160dbc945bf9
PraveenKumar <159308991+unique6246@users.noreply.github.com>
2024-06-19 11:56:31 +0530

    completed

--- app/src/main/java/com/example/messagingapp/LoginActivity.kt
+++ app/src/main/java/com/example/messagingapp/LoginActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
